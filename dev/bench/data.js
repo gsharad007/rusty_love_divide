eea1de48b36ab596a2de02732ae9fe2da7504bfc
github-action-benchmark <github@users.noreply.github.com>
2021-12-31 08:04:42 +0000

    add bench-Linux (cargo) benchmark result for 9919551f5d768b4f94c420a5021f912333015abf

---
+++ dev/bench/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1640937882500,
+  "repoUrl": "https://github.com/gsharad007/rusty_love_divide",
+  "entries": {
+    "bench-Linux": [
+      {
+        "commit": {
+          "author": {
+            "email": "sharad@unknownworlds.com",
+            "name": "THESHREDDER\\TheBeast"
+          },
+          "committer": {
+            "email": "sharad@unknownworlds.com",
+            "name": "THESHREDDER\\TheBeast"
+          },
+          "distinct": true,
+          "id": "9919551f5d768b4f94c420a5021f912333015abf",
+          "message": "Working benchmarking now",
+          "timestamp": "2021-12-31T02:49:36-05:00",
+          "tree_id": "5200407794e22db9bfc9fd82a47dc7fbb76aaa5e",
+          "url": "https://github.com/gsharad007/rusty_love_divide/commit/9919551f5d768b4f94c420a5021f912333015abf"
+        },
+        "date": 1640937881823,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "game::deck_generator::tests_generating_permutations::bench_generate_all_permutations_of_square_tiles_with_4_edge_types",
+            "value": 13480,
+            "range": "± 53",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
